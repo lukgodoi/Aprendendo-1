aa9ef942dfcfde4e260679206a32ae2b80172a93
Lucas <anywaytanto@gmail.com>
2025-06-04 07:59:00 -0300

    Add files via upload
    Aprendendo a utilizar variáveis

---
+++ Variáveis.py
@@ -0,0 +1,7 @@
+Nome = "Lucas"
+Idade = 22
+Peso = 59.6
+print(f"Olá, {Nome}! Você tem {Idade} anos e pesa {Peso} kg.")
+Idade += 5
+Peso += 3.5
+print(f"Olá, {Nome}! Você tem {Idade} anos e pesa {Peso} kg.")
